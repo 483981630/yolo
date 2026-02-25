07dbd8cdefa033dcd4bd482d6e2e346ca0a1ccc3
home <483981630@qq.com>
2026-02-25 22:20:42 +0800

    提交

---
+++ ultralytics-8.3.163/mycam.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO(r"yolov8s.pt")
+results = model(
+    source=0,
+    stream=True,
+)
+
+for result in results:
+    plotted = result.plot()
+    cv2.imshow("yolo inference", plotted)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
---
+++ ultralytics-8.3.163/mytest.py
@@ -0,0 +1,4 @@
+from ultralytics import YOLO
+
+model =YOLO(r"yolo11s.pt")
+print(model.task)
